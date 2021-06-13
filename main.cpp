a0dfa3487997ee3cbd9444dfa36948a3172dbfc2
nikhil <nikhilvs555@gmail.com>
2021-06-13 13:57:34 +0530

    Added argments

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
 // first program to indetify the git command
-int main()
+int main(int argc,char * argv[])
 {
-	cout<<"Hello World";
+	
+	cout<<"Hello World ";
+	if(argc>1)
+		cout<<argv[1];
 	return 1;
 }
